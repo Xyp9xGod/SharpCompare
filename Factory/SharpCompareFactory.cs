3981a5ffff0fe60a89d84d53ebd95c242810fca3
Arilson <arilson.jlle@gmail.com>
2025-03-12 00:31:07 -0300

    Add factory to hide the implementation and expose only the interface

---
+++ Factory/SharpCompareFactory.cs
@@ -0,0 +1,21 @@
+﻿using SharpCompare.Interfaces;
+using SharpCompare.Services;
+
+namespace SharpCompare.Factory
+{
+    /// <summary>
+    /// Factory for creating instances of SharpCompare.
+    /// </summary>
+    public static class SharpCompareFactory
+    {
+        /// <summary>
+        /// Creates a default implementation of <see cref="ISharpCompare"/>.
+        /// </summary>
+        /// <param name="useDFS">Defines whether to use DFS-based comparison (default: true).</param>
+        /// <returns>An instance of <see cref="ISharpCompare"/>.</returns>
+        public static ISharpCompare Create(bool useDFS = true)
+        {
+            return useDFS ? new SharpCompareDFSService() : new SharpCompareReflectionService();
+        }
+    }
+}
--- Interfaces/ISharpCompare.cs
+++ Interfaces/ISharpCompare.cs
@@ -2,6 +2,19 @@
 {
     public interface ISharpCompare
     {
+        /// <summary>
+        /// Compares two objects by their properties and values, ignoring memory references.
+        /// </summary>
+        /// <param name="firstObject">The first object to compare.</param>
+        /// <param name="secondObject">The second object to compare.</param>
+        /// <returns>
+        /// Returns <c>true</c> if both objects have the same property values; otherwise, returns <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// - Supports deep comparison of nested objects.
+        /// - Can compare collections like lists and dictionaries.
+        /// - Allows ignoring specific properties using the <see cref="IgnoreComparisonAttribute"/>.
+        /// </remarks>
         bool IsEqual(object firstObject, object secondObject);
     }
 }
