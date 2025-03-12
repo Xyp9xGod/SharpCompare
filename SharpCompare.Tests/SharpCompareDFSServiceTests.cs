ec93a282a2fcc8f12f9c5575ea7a1eb75aa78970
Arilson <arilson.jlle@gmail.com>
2025-03-12 11:08:04 -0300

    reestructuing solution

---
+++ SharpCompare.Tests/SharpCompareDFSServiceTests.cs
@@ -0,0 +1,175 @@
+﻿using SharpCompare.Services;
+using SharpCompare.Extensions;
+using SharpCompare.Interfaces;
+using SharpCompare.Factory;
+
+namespace SharpCompare.Tests
+{
+    public class SharpCompareDFSServiceTests
+    {
+        private readonly ISharpCompare _comparer = SharpCompareFactory.Create(useDFS: true);
+
+        private class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Address Address { get; set; }
+
+            [IgnoreComparison]
+            public string Ignored { get; set; }
+        }
+
+        private class Address
+        {
+            public string Street { get; set; }
+            public int Number { get; set; }
+        }
+
+        [Fact]
+        public void ObjectsAreEqual_ShouldReturnTrue()
+        {
+            var person1 = new Person { Name = "John", Age = 30, Address = new Address { Street = "Street A", Number = 100 } };
+            var person2 = new Person { Name = "John", Age = 30, Address = new Address { Street = "Street A", Number = 100 } };
+
+            bool result = _comparer.IsEqual(person1, person2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ObjectsAreDifferent_ShouldReturnFalse()
+        {
+            var person1 = new Person { Name = "John", Age = 30, Address = new Address { Street = "Street A", Number = 100 } };
+            var person2 = new Person { Name = "Carlos", Age = 25, Address = new Address { Street = "Street B", Number = 200 } };
+
+            bool result = _comparer.IsEqual(person1, person2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ObjectsWithIgnoredProperty_ShouldBeEqual()
+        {
+            var person1 = new Person { Name = "John", Age = 30, Ignored = "Value1" };
+            var person2 = new Person { Name = "John", Age = 30, Ignored = "Value2" };
+
+            bool result = _comparer.IsEqual(person1, person2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ObjectsWithNullProperties_ShouldBeEqual()
+        {
+            var person1 = new Person { Name = "John", Age = 30, Address = null };
+            var person2 = new Person { Name = "John", Age = 30, Address = null };
+
+            bool result = _comparer.IsEqual(person1, person2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CompareObjectWithNull_ShouldReturnFalse()
+        {
+            var person1 = new Person { Name = "John", Age = 30 };
+            Person person2 = null;
+
+            bool result = _comparer.IsEqual(person1, person2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CompareEqualCollections_ShouldReturnTrue()
+        {
+            var list1 = new List<Person>
+            {
+                new Person { Name = "John", Age = 30 },
+                new Person { Name = "Maria", Age = 25 }
+            };
+
+            var list2 = new List<Person>
+            {
+                new Person { Name = "John", Age = 30 },
+                new Person { Name = "Maria", Age = 25 }
+            };
+
+            bool result = _comparer.IsEqual(list1, list2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CompareDifferentCollections_ShouldReturnFalse()
+        {
+            var list1 = new List<Person>
+            {
+                new Person { Name = "John", Age = 30 },
+                new Person { Name = "Carlos", Age = 40 }
+            };
+
+            var list2 = new List<Person>
+            {
+                new Person { Name = "John", Age = 30 },
+                new Person { Name = "Maria", Age = 25 }
+            };
+
+            bool result = _comparer.IsEqual(list1, list2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CompareEqualDictionaries_ShouldReturnTrue()
+        {
+            var dict1 = new Dictionary<string, Person>
+            {
+                { "A", new Person { Name = "John", Age = 30 } },
+                { "B", new Person { Name = "Maria", Age = 25 } }
+            };
+
+            var dict2 = new Dictionary<string, Person>
+            {
+                { "A", new Person { Name = "John", Age = 30 } },
+                { "B", new Person { Name = "Maria", Age = 25 } }
+            };
+
+            bool result = _comparer.IsEqual(dict1, dict2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CompareDifferentDictionaries_ShouldReturnFalse()
+        {
+            var dict1 = new Dictionary<string, Person>
+            {
+                { "A", new Person { Name = "John", Age = 30 } },
+                { "B", new Person { Name = "Carlos", Age = 40 } }
+            };
+
+            var dict2 = new Dictionary<string, Person>
+            {
+                { "A", new Person { Name = "John", Age = 30 } },
+                { "B", new Person { Name = "Maria", Age = 25 } }
+            };
+
+            bool result = _comparer.IsEqual(dict1, dict2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CompareDifferentTypes_ShouldReturnFalse()
+        {
+            var person = new Person { Name = "John", Age = 30 };
+            var address = new Address { Street = "Street A", Number = 100 };
+
+            bool result = _comparer.IsEqual(person, address);
+
+            Assert.False(result);
+        }
+    } 
+}
+
---
+++ SharpCompare.Tests/SharpCompareReflectionServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using SharpCompare.Extensions;
+using SharpCompare.Factory;
+using SharpCompare.Interfaces;
+
+namespace SharpCompare.Tests
+{
+    public class SharpCompareReflectionServiceTests
+    {
+        private readonly ISharpCompare _comparer = SharpCompareFactory.Create(useDFS: false);
+
+        private class Pessoa
+        {
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+            [IgnoreComparison]
+            public string Senha { get; set; }
+        }
+
+        private class Familia
+        {
+            public string Sobrenome { get; set; }
+            public List<Pessoa> Membros { get; set; }
+        }
+
+        [Fact]
+        public void IsEqual_SameValues_ReturnsTrue()
+        {
+            var pessoa1 = new Pessoa { Nome = "João", Idade = 30 };
+            var pessoa2 = new Pessoa { Nome = "João", Idade = 30 };
+
+            Assert.True(_comparer.IsEqual(pessoa1, pessoa2));
+        }
+
+        [Fact]
+        public void IsEqual_DifferentValues_ReturnsFalse()
+        {
+            var pessoa1 = new Pessoa { Nome = "João", Idade = 30 };
+            var pessoa2 = new Pessoa { Nome = "Maria", Idade = 25 };
+
+            Assert.False(_comparer.IsEqual(pessoa1, pessoa2));
+        }
+
+        [Fact]
+        public void IsEqual_NullObject_ReturnsFalse()
+        {
+            var pessoa = new Pessoa { Nome = "João", Idade = 30 };
+
+            Assert.False(_comparer.IsEqual(pessoa, null));
+        }
+
+        [Fact]
+        public void IsEqual_DifferentTypes_ReturnsFalse()
+        {
+            var pessoa = new Pessoa { Nome = "João", Idade = 30 };
+            var outraClasse = new { Nome = "João", Idade = 30 };
+
+            Assert.False(_comparer.IsEqual(pessoa, outraClasse));
+        }
+
+        [Fact]
+        public void IsEqual_IgnoreProperty_ReturnsTrue()
+        {
+            var pessoa1 = new Pessoa { Nome = "João", Idade = 30, Senha = "1234" };
+            var pessoa2 = new Pessoa { Nome = "João", Idade = 30, Senha = "5678" };
+
+            Assert.True(_comparer.IsEqual(pessoa1, pessoa2));
+        }
+
+        [Fact]
+        public void IsEqual_CompareLists_ReturnsTrue()
+        {
+            var familia1 = new Familia
+            {
+                Sobrenome = "Silva",
+                Membros = new List<Pessoa>
+                {
+                    new Pessoa { Nome = "João", Idade = 30 },
+                    new Pessoa { Nome = "Maria", Idade = 25 }
+                }
+            };
+
+            var familia2 = new Familia
+            {
+                Sobrenome = "Silva",
+                Membros = new List<Pessoa>
+                {
+                    new Pessoa { Nome = "João", Idade = 30 },
+                    new Pessoa { Nome = "Maria", Idade = 25 }
+                }
+            };
+
+            Assert.True(_comparer.IsEqual(familia1, familia2));
+        }
+
+        [Fact]
+        public void IsEqual_CompareLists_ReturnsFalse()
+        {
+            var familia1 = new Familia
+            {
+                Sobrenome = "Silva",
+                Membros = new List<Pessoa>
+                {
+                    new Pessoa { Nome = "João", Idade = 30 },
+                    new Pessoa { Nome = "Maria", Idade = 25 }
+                }
+            };
+
+            var familia2 = new Familia
+            {
+                Sobrenome = "Silva",
+                Membros = new List<Pessoa>
+                {
+                    new Pessoa { Nome = "João", Idade = 30 },
+                    new Pessoa { Nome = "Carlos", Idade = 40 }
+                }
+            };
+
+            Assert.False(_comparer.IsEqual(familia1, familia2));
+        }
+    }
+}
